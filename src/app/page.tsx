13328af1cd20a0874151801075037e3a3883f33c
saadfrhan <saadfarhan347@gmail.com>
2023-06-29 07:45:51 +0000

    feat: complete homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,9 @@ import FeaturedOne from '/public/Featured1.webp';
 import FeaturedTwo from '/public/Featured2.webp';
 import FeaturedThree from '/public/Featured3.webp';
 import FeaturedFour from '/public/Featured4.webp';
+
+import Feature from '/public/feature.webp'
+
 import HeaderImage from '/public/header.png'
 
 import EventOne from '/public/Event1.webp';
@@ -219,5 +222,53 @@ export default function Home() {
           </div>
       </Swiper>
     </div>
+    <section className="features-section">
+      <div className="title">
+        <h1>Unique and Authentic Vintage Designer Jewellery</h1>
+      </div>
+      <div className="content">
+        <div className="left">
+          <div className="feature-background">Different from others</div>
+        <div>
+          <h3>Using Good Quality Materials</h3>
+          <p>Lorem ipsum dolor sit amt, consectetus adipiscing elit.</p>
+        </div>
+        <div>
+          <h3>Using Good Quality Materials</h3>
+          <p>Lorem ipsum dolor sit amt, consectetus adipiscing elit.</p>
+        </div>
+        <div>
+          <h3>Using Good Quality Materials</h3>
+          <p>Lorem ipsum dolor sit amt, consectetus adipiscing elit.</p>
+        </div>
+        <div>
+          <h3>Using Good Quality Materials</h3>
+          <p>Lorem ipsum dolor sit amt, consectetus adipiscing elit.</p>
+        </div>
+        </div>
+        <div className="right">
+          <Image src={Feature} alt="Feature" width={300} height={350} />
+          <div>
+            <p>
+            This piece is ethically crafted in our small family-owned workshop in Peru with unmatched attention to detail and care. The Natural color is the actual natural color of the fiber, undyed and 100% traceable.
+            </p>
+            <Link href="/products">
+                <button className='btn'>See All Products</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section className="newsletter">
+      <div className="newsletter-background">
+        Newsletter
+      </div>
+      <h1>Subscribe Our Newsletter</h1>
+      <p>Get the latest information and promo offers directly</p>
+      <form>
+        <input type="email" placeholder='Input email address' />
+        <button type="submit">Get Started</button>
+      </form>
+    </section>
   </div>
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -36,7 +36,7 @@ function MobileNavbar() {
       <div className={`
       ${showOverlay && `h-screen flex flex-col bg-white items-center justify-center fixed top-0 left-0 w-full z-[3]`}
       `}>
-      {!showOverlay && <div className="absolute max-[650px]:top-8 max-[650px]:right-8 cursor-pointer max-[1000px]:top-16 max-[1000px]:right-16" ><AlignRight width="40" height="30" onClick={() => setShowOverlay(true)} /></div>}
+      {!showOverlay && <div className="absolute max-[650px]:top-8 max-[650px]:right-8 cursor-pointer max-[1000px]:top-16 z-[1] max-[1000px]:right-16" ><AlignRight className='cursor-pointer' width="40" height="30" onClick={() => setShowOverlay(true)} /></div>}
       <div>
         <div className="flex justify-between items-center m-auto">
           <div className='absolute max-[650px]:top-8 max-[650px]:left-8 cursor-pointer max-[1000px]:top-16 max-[1000px]:left-16'>
