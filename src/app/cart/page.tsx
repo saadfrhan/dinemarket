c639e540f52b8e753781ac0d5e9b42aa6179b33a
saadfrhan <saadfarhan347@gmail.com>
2023-07-05 16:13:58 +0000

    feat: complete cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 import ProductTwo from '/public/big.png';
+import { MinusIcon, PlusIcon, Trash2Icon } from 'lucide-react';
 
 export default function Cart() {
   return (
@@ -10,28 +11,34 @@ export default function Cart() {
         <div className="cart-items">
           <div className="item-card">
             <div className="item-image">
-              {/* <Image /> */}
+              <Image src={ProductTwo} alt="somethin truly special" />
             </div>
             <div className="item-details">
               <div className="name-and-remove">
                 <h3>Flex Sweatpants</h3>
-                <button className="remove-item"></button>
+                <button className="remove-item">
+                  <Trash2Icon width={28} height={28} />
+                </button>
               </div>
               <p className="item-tag">Dress</p>
               <p className="delivery-est">Delivery Estimation</p>
               <p className="delivery-days">5 Working Days</p>
               <div className="price-and-qty">
-                <span className="price"></span>
+                <span className="price">$145</span>
                 <div>
-                  <span className="minus"></span>
+                  <span className="minus">
+                    <MinusIcon />
+                  </span>
                   <span className="num">1</span>
-                  <span className="plus"></span>
+                  <span className="plus">
+                    <PlusIcon />
+                  </span>
                 </div>
               </div>
             </div>
           </div>
         </div>
-         <div className="order-summary">
+        <div className="order-summary">
           <h3>Order Summary</h3>
           <div className="qty">
             <p>Quantity</p>
