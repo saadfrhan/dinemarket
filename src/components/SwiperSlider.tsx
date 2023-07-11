db603709fb20f7d97850afafa40d158771ee9cbb
saadfrhan <saadfarhan347@gmail.com>
2023-07-11 09:04:11 +0500

    feat: update

--- src/components/SwiperSlider.tsx
+++ src/components/SwiperSlider.tsx
@@ -1,53 +1,7 @@
-"use client";
-
-import { Navigation, A11y } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
 import { getProducts } from '../../sanity/utils';
-import ProductSlideCard from './ProductSlideCard';
-import { urlForImage } from '../../sanity/lib/image';
+import SwiperSliderContainer from './SwiperSliderContainer';
 
 export default async function SwiperSlider() {
   const products = await getProducts('all');
-  return (
-    <Swiper
-      breakpoints={{
-        // width >= 300
-        300: {
-          slidesPerView: 1,
-          spaceBetween: 100
-        },
-        // width >= 1000
-        1000: {
-          slidesPerView: 2,
-          spaceBetween: 0
-        },
-        // width >= 1260
-        1260: {
-          slidesPerView: 3,
-          spaceBetween: 0
-        }
-      }}
-      modules={[Navigation, A11y]}
-      spaceBetween={0}
-      slidesPerView={3}
-      navigation
-    >
-      <div className='products-container'>
-        {products.map(({ name, price, slug, image }, index) => (
-          <SwiperSlide key={index}>
-            <ProductSlideCard
-              imageUrl={urlForImage(image && image[0]).url()}
-              name={name}
-              price={price}
-              slug={slug}
-            />
-          </SwiperSlide>
-        ))}
-      </div>
-    </Swiper>
-  )
+  return <SwiperSliderContainer products={products} />
 }
---
+++ src/components/SwiperSliderContainer.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { Navigation, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import ProductSlideCard from './ProductSlideCard';
+import { urlForImage } from '../../sanity/lib/image';
+import { ProductCardI } from '@/types';
+
+export default function SwiperSliderContainer(
+    {products}:
+    {products: ProductCardI[]}
+) {
+  return (
+    <Swiper
+      breakpoints={{
+        // width >= 300
+        300: {
+          slidesPerView: 1,
+          spaceBetween: 100
+        },
+        // width >= 1000
+        1000: {
+          slidesPerView: 2,
+          spaceBetween: 0
+        },
+        // width >= 1260
+        1260: {
+          slidesPerView: 3,
+          spaceBetween: 0
+        }
+      }}
+      modules={[Navigation, A11y]}
+      spaceBetween={0}
+      slidesPerView={3}
+      navigation
+    >
+      <div className='products-container'>
+        {products.map(({ name, price, slug, image }, index) => (
+          <SwiperSlide key={index}>
+            <ProductSlideCard
+              imageUrl={urlForImage(image && image[0]).url()}
+              name={name}
+              price={price}
+              slug={slug}
+            />
+          </SwiperSlide>
+        ))}
+      </div>
+    </Swiper>
+  )
+}
